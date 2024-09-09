460b84680887483562de105769bc8c1175d3c12b
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 17:34:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const puppeteer = require('puppeteer');
 
   const page = await browser.newPage();
 
+    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+  await page.setUserAgent(userAgent);
+  
   // Define the URL you want to request
   const url = 'https://gota.io';
 
