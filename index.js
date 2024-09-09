02bbaa07b8cd8d963240181bbf4b6134e30bc8d3
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 17:44:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const HttpsProxyAgent = require("https-proxy-agent");
 
 const url = "https://gota.io";
 const proxy = "http://1b072891695ddc068abbd73daecb42d704f9059c@api.zenrows.com:8001";
-const httpAgent = new HttpProxyAgent(proxy);
 const httpsAgent = new HttpsProxyAgent(proxy);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 axios({
 	url,
-	httpAgent,
 	httpsAgent,
 	method: 'GET',
 })
