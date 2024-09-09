86160bbcfa43543a5728db0608e383d150c54a93
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 17:35:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
 
 (async () => {
-  // Launch a new browser instance with no-sandbox and headless options
   const browser = await puppeteer.launch({
-    headless: true,          // Run in headless mode
-    args: ['--no-sandbox']   // Add the no-sandbox argument
+    headless: false,
+    targetFilter: (target) => target.type() !== "other",
   });
 
   const page = await browser.newPage();
+  await page.goto("https://gota.io");
 
-    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
-  await page.setUserAgent(userAgent);
-  
-  // Define the URL you want to request
-  const url = 'https://gota.io';
-
-  // Navigate to the URL (GET request)
-
-  const response = await page.goto(url, { waitUntil: 'networkidle2' });
-  
-const headers = response.headers();
-console.log(headers);
-
-console.log(response.status());
-  
-
-  // Close the browser
-  await browser.close();
 })();
