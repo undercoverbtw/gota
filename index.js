b5ae23fb0de9f8afc26b12725bde6ea183ab4bdc
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 17:44:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+// npm install axios http-proxy-agent https-proxy-agent
+const axios = require("axios");
+const HttpProxyAgent = require("http-proxy-agent");
+const HttpsProxyAgent = require("https-proxy-agent");
 
-puppeteer.use(StealthPlugin());
-
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-       args: ['--no-sandbox'],
-    targetFilter: (target) => target.type() !== "other",
-  });
-
-  const page = await browser.newPage();
- // Navigate to the URL (GET request)
-  const response = await page.goto("https://gota.io/web", { waitUntil: 'networkidle2' });
-
-  // Get and print the status code and page content
-  console.log('Status Code:', response.status());
-
-    const title = await page.title();
-    console.log("Page title:", title);
-    await browser.close();
-  
-})();
+const url = "https://gota.io";
+const proxy = "http://1b072891695ddc068abbd73daecb42d704f9059c@api.zenrows.com:8001";
+const httpAgent = new HttpProxyAgent(proxy);
+const httpsAgent = new HttpsProxyAgent(proxy);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+axios({
+	url,
+	httpAgent,
+	httpsAgent,
+	method: 'GET',
+})
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error));
