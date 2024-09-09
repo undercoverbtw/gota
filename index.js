e2175c2402d3e79f3ffed5013fe832dbf70a4913
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 18:12:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 
 const username = 'm4teelias';
 const password = 'Diana110604'; 
-const agent = new HttpsProxyAgent(`https://${username}:${password}@unblock.oxylabs.io:60000`);/
+const agent = new HttpsProxyAgent(`https://${username}:${password}@unblock.oxylabs.io:60000`);
 // Ignore the certificate 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; 
 const response = 
